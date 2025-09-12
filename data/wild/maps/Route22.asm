98abf5b5a0721a4503c70b3d34c5466d1b7e88c0
CreamElDudJafar <jahern2398@gmail.com>
2025-09-12 09:46:52 -0500

    Added Poliwag To Route 22

--- data/wild/maps/Route22.asm
+++ data/wild/maps/Route22.asm
@@ -7,9 +7,9 @@ Route22WildMons:
 	db  4, NIDORAN_M
 	db  4, NIDORAN_F
 	db  5, MANKEY
-	db  2, SPEAROW
 	db  4, SPEAROW
-	db  6, SPEAROW
+	db  5, SPEAROW
+	db  3, POLIWAG
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
