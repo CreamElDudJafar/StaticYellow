e9aaa7973a477acefda5bf3f03ee3a1529b2a0d7
CreamElDudJafar <jahern2398@gmail.com>
2025-10-04 08:35:40 -0500

    Re-Added Missing Mt Moon Trainer And Placed In B1F

--- text/MtMoonB1F.asm
+++ text/MtMoonB1F.asm
@@ -1,3 +1,21 @@
+_MtMoonB1FRocketBattleText::
+	text "TEAM ROCKET will"
+	line "find the fossils,"
+	cont "revive and sell"
+	cont "them for cash."
+	done
+
+_MtMoonB1FRocketEndBattleText::
+	text "Urgh!"
+	line "Now I'm mad!"
+	prompt
+
+_MtMoonB1FRocketAfterBattleText::
+	text "You made me mad!"
+	line "TEAM ROCKET will"
+	cont "blacklist you!"
+	done
+
 ;_MtMoonB1FUnusedText::
 ;	text_start
 ;	done
