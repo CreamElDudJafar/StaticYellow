4f7b9c4eaef8e88aa8bcff81d507d42774712d67
CreamElDudJafar <jahern2398@gmail.com>
2025-11-16 16:44:43 -0600

    Update Wild Encounters

--- data/wild/maps/PokemonMansion2F.asm
+++ data/wild/maps/PokemonMansion2F.asm
@@ -1,13 +1,13 @@
 PokemonMansion2FWildMons:
 	def_grass_wildmons 10 ; encounter rate
-	db 37, RATTATA
-	db 37, RATICATE
+	db 40, RATTATA
+	db 40, RATICATE
 	db 36, GRIMER
 	db 39, GRIMER
 	db 40, MUK
-	db 40, RATICATE
 	db 40, MUK
-	db 35, GROWLITHE
+	db 36, GROWLITHE
+	db 38, GROWLITHE
 	db 42, MAGMAR
 	db 45, MAGMAR
 	end_grass_wildmons
--- data/wild/maps/Route7.asm
+++ data/wild/maps/Route7.asm
@@ -1,15 +1,15 @@
 Route7WildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 20, KOFFING
-	db 22, PIDGEY
 	db 20, EKANS
-	db 15, ABRA
+	db 20, ABRA
+	db 22, VULPIX
 	db 19, MEOWTH
+	db 20, KOFFING
 	db 24, PIDGEOTTO
-	db 26, PONYTA
-	db 20, JIGGLYPUFF
+	db 26, JIGGLYPUFF
+	db 19, PONYTA
 	db 25, JIGGLYPUFF
-	db 25, JIGGLYPUFF
+	db 30, JIGGLYPUFF
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
