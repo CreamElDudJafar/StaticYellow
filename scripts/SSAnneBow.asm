bfbf1b49ebf2d1b25c767a1ab1b578211669652f
CreamElDudJafar <jahern2398@gmail.com>
2025-11-19 19:23:09 -0600

    Add Secret Money Character on SS Anne Bow
    Gives Player 1,000 dollars.

--- scripts/SSAnneBow.asm
+++ scripts/SSAnneBow.asm
@@ -20,6 +20,7 @@ SSAnneBow_TextPointers:
 	dw_const SSAnneBowCooltrainerMText, TEXT_SSANNEBOW_COOLTRAINER_M
 	dw_const SSAnneBowSailor2Text,      TEXT_SSANNEBOW_SAILOR2
 	dw_const SSAnneBowSailor3Text,      TEXT_SSANNEBOW_SAILOR3
+	dw_const SSAnneBowSuperNerd2Text,   TEXT_SSANNEBOW_SUPER_NERD_2
 
 SSAnne5TrainerHeaders:
 	def_trainers 4
@@ -39,6 +40,39 @@ SSAnneBowSailor1Text:
 
 SSAnneBowCooltrainerMText:
 	text_far _SSAnneBowCooltrainerMText
+	text_end
+
+SSAnneBowSuperNerd2Text:
+	text_asm
+	CheckEvent EVENT_SS_ANNE_FREEMONEY
+	jr nz, .GotMoney
+	ld hl, .Text1
+	rst _PrintText
+	ld hl, .Text2
+	call PrintText
+	SetEvent EVENT_SS_ANNE_FREEMONEY
+	ld hl, $1000
+	ld a, l
+	ld [wAmountMoneyWon + 2], a
+	ld a, h
+	ld [wAmountMoneyWon + 1], a
+; Add the 1000 money to the player's total
+	ld de, wPlayerMoney + 2
+	ld hl, wAmountMoneyWon + 2
+	ld c, $3
+	predef_jump AddBCDPredef
+.GotMoney
+	ld hl, .Text3
+	rst _PrintText
+	rst TextScriptEnd
+.Text1
+	text_far _SSAnneBowSuperNerd1Text
+	text_end
+.Text2
+	text_far _SSAnneBowSuperNerd2Text
+	text_end
+.Text3
+	text_far _SSAnneBowSuperNerd3Text
 	text_end
 
 SSAnneBowSailor2Text:
