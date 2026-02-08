de2f3f5389ddcc73b7655e1e848ad1ccc7993ac1
CreamElDudJafar <jahern2398@gmail.com>
2026-02-08 09:22:40 -0600

    Update Warps From Last Commit
    Warps for each map need to line up number wise

--- data/maps/objects/SilphCo7F.asm
+++ data/maps/objects/SilphCo7F.asm
@@ -18,7 +18,7 @@ SilphCo7F_Object:
 	warp_event 16,  0, SILPH_CO_8F, 2
 	warp_event 22,  0, SILPH_CO_6F, 1
 	warp_event 18,  0, SILPH_CO_ELEVATOR, 1
-	warp_event  5,  7, SILPH_CO_11F, 4
+	warp_event  5,  7, SILPH_CO_11F, 3
 	warp_event  5,  3, SILPH_CO_3F, 9
 	warp_event 21, 15, SILPH_CO_5F, 4
 
